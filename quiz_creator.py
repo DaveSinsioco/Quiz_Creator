840729011c72035c8f82b4841de9d03313e55166
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 21:37:49 +0800

    initializes the questions, choices, and answers in appendices

---
+++ quiz_creator.py
@@ -0,0 +1,17 @@
+# initializes the questions, choices, and answers in appendices
+questions = []
+first_choices = []
+second_choices = []
+third_choices = []
+fourth_choices = []
+answers = []
+
+while True:
+
+    # asks the user to enter question, choices, answer
+    question = input("Enter the question: ")
+    choice1 = input("Enter the choice A: ")
+    choice2 = input("Enter the choice B: ")
+    choice3 = input("Enter the choice C: ")
+    choice4 = input("Enter the choice D: ")
+    answer = input("Enter the correct choice (A/B/C/D): ")
