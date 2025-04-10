8248b733beadac4acce12b50f64909a29857cfee
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 22:01:28 +0800

    debugging and finalizing the code

--- quiz_creator.py
+++ quiz_creator.py
@@ -19,16 +19,16 @@ while True:
     questions.append(question)
 
     first_choice = input("Enter the choice A: ")
-    first_choices.append(choice1)
+    first_choices.append(first_choice)
 
     second_choice = input("Enter the choice B: ")
-    second_choices.append(choice2)
+    second_choices.append(second_choice)
 
     third_choice = input("Enter the choice C: ")
-    third_choices.append(choice3)
+    third_choices.append(third_choice)
 
     fourth_choice = input("Enter the choice D: ")
-    fourth_choices.append(choice4)
+    fourth_choices.append(fourth_choice)
 
     answer = input("Enter the correct choice (A/B/C/D): ")
     answers.append(answer)
